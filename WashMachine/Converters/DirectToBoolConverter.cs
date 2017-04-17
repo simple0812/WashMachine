d11d3b5391813e9dc0ccaff2e835f8c0186c3e91
zhanglei <lei.zhang@sunscell.com>
2017-04-17 14:56:02 +0800

    merge

---
+++ WashMachine/Converters/DirectToBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using WashMachine.Enums;
+
+namespace WashMachine.Converters
+{
+    class DirectToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var val = System.Convert.ToInt32(value);
+            return val == (int)DirectionEnum.Out;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
